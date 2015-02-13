454c01e3f1caeb3edc278dfdc35269c9ae71a0b2
Phil Meads <phil.meads@gmail.com>
2015-02-12 16:32:15 -0800

    add is rent due on index

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -21,8 +21,49 @@ class Stage < ActiveRecord::Base
     end
 
 	def rent_due?
-	
-	  nil
+	  rent_is_due=false
+	  if in_extended_rental?
+	  	# rent has never been paid, enter a row for rent due
+	  	rents_count = rents.size
+	  	puts "rents_count=#{rents_count.to_s}"
+        if rents_count == 0
+        	Rent.transaction do
+	        	rent_due_on = stage_date.advance(:months => 2)
+	        	r = Rent.new
+	        	r.property_id = property_id
+	        	r.stage_id = id
+	        	r.rent_due = monthly_rental
+	        	r.rent_due_on = rent_due_on
+	        	r.save
+	        	rent_is_due = true
+	        end
+
+        else
+
+           rent_due_on = stage_date.advance(:months => rents_count + 2)
+           puts "rent_due_on = #{rent_due_on.to_s}"
+           r = Rent.where("stage_id = #{id} and rent_due_on = ?", rent_due_on)
+           if r.size == 0 
+	        	Rent.transaction do
+		        	rent_due_on = stage_date.advance(:months => rents_count + 2)
+		        	r = Rent.new
+		        	r.property_id = property_id
+		        	r.stage_id = id
+		        	r.rent_due = monthly_rental
+		        	r.rent_due_on = rent_due_on
+		        	r.save
+		        	rent_is_due = true
+		        end
+           
+           elsif r.first.rent_paid.nil?
+           	 rent_is_due = true
+           end
+
+        end
+
+	  end
+
+      rent_is_due
 
 	end
 
--- db/rents.sql
+++ db/rents.sql
@@ -4,12 +4,18 @@ CREATE TABLE "rents" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
   "property_id" integer(11) NOT NULL, 
   "stage_id" integer(11), 
-  "transaction_date" datetime, 
-  "rent_due" decimal(5,2) default 0, 
-  "rent_paid" decimal(5,2) default 0, 
+  "rent_due" decimal(5,2), 
+  "rent_due_on" datetime,
+  "rent_paid" decimal(5,2), 
+  "rent_paid_on" datetime,
+  "rent_payment_method" varchar(8),
   "rent_days" integer(2) default 0, 
-  "refund_amount" decimal(5,2) default 0 
+  "refunded_amount" decimal(5,2) default 0,
+  "refunded_on" datetime,
+  "refunded_days" integer(2)
   );
 CREATE INDEX "rents_ni1" ON "rents" ("property_id");
 CREATE INDEX "rents_ni2" ON "rents" ("stage_id");
+CREATE INDEX "rents_ni3" ON "rents" ("stage_id","rent_due_on");
+CREATE INDEX "rents_ni4" ON "rents" ("stage_id","rent_due_on","rent_paid");
 COMMIT;
